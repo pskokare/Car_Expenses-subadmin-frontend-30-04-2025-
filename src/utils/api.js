2f4b1efda462d100db198531685eaa9408d3b8d7
pragatikokare32@gmail.com <pragatikokare32@gmail.com>
2025-04-22 13:43:21 +0530

    new updated

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-// const baseURL = "https://api.expengo.com/";
+const baseURL = "https://api.expengo.com/";
 // const baseURL = "http://localhost:5000/";
-const baseURL = "https://car-expenses-backend.vercel.app/"
+// const baseURL = "https://car-expenses-backend.vercel.app/"
 export default baseURL;
