eb6a203a586b63c7db8fdd0d47ba799bbc644154
pragatikokare32@gmail.com <pragatikokare32@gmail.com>
2025-05-12 13:23:34 +0530

    updated frontend

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Cab Expengo",
+  title: "Cab Root Budget",
   description: "",
 };
 
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-const baseURL = "https://api.expengo.com/";
+const baseURL = "https://api.Root Budget.com/";
 // const baseURL = "http://localhost:5000/";
 // const baseURL = "https://car-expenses-backend.vercel.app/"
 export default baseURL;
