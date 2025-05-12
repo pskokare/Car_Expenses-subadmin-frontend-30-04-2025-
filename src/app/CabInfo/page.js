4145628f310fe8e13ad3eca2b1ee6fdf0ab43c57
pragatikokare32@gmail.com <pragatikokare32@gmail.com>
2025-05-12 13:46:17 +0530

    updated frontend

--- src/app/CabInfo/page.js
+++ src/app/CabInfo/page.js
@@ -414,7 +414,7 @@ const CabSearch = () => {
     const connectWebSocket = () => {
       if (wsRef.current) return;
   
-      const wsUrl = "wss://api.RootBudget.com/";
+      const wsUrl = "wss://api.expengo.com/";
       wsRef.current = new WebSocket(wsUrl);
   
       wsRef.current.onopen = () => {
